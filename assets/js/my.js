8fb1ff32ce3fe7e54c63b5c906fcadb87f57c55e
Florian <fanzhidongyzby@gmail.com>
2013-07-19 23:33:41 +0800

    init

---
+++ assets/js/my.js
@@ -0,0 +1,44 @@
+!function ($) {
+
+  $(function(){
+
+    var $window = $(window)
+
+    // side bar
+    $('.mysidenav').affix({
+      offset: {
+        top: 10
+      }
+    })
+
+    // request built javascript
+    $('.download-btn').on('click', function () {
+
+      var css = $("#components.download input:checked")
+            .map(function () { return this.value })
+            .toArray()
+        , js = $("#plugins.download input:checked")
+            .map(function () { return this.value })
+            .toArray()
+        , vars = {}
+        , img = ['glyphicons-halflings.png', 'glyphicons-halflings-white.png']
+
+    $("#variables.download input")
+      .each(function () {
+        $(this).val() && (vars[ $(this).prev().text() ] = $(this).val())
+      })
+
+      $.ajax({
+        type: 'POST'
+      , url: /\?dev/.test(window.location) ? 'http://localhost:3000' : 'http://bootstrap.herokuapp.com'
+      , dataType: 'jsonpi'
+      , params: {
+          js: js
+        , css: css
+        , vars: vars
+        , img: img
+      }
+      })
+    })
+  })
+}(window.jQuery)
